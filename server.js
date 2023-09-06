085cb1c05f532ac69e01442b83ae98880187e220
qvadricepc <qvadricepc@gmail.com>
2023-09-06 15:54:16 +0300

    feat: node js task 2

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 require("dotenv").config();
 const boom = require("@hapi/boom");
 const errorHandler = require("./middleware/error-handler");
@@ -9,7 +8,6 @@ const errors = require("./routes/errors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
 app.use(express.json());
 
 app.get("/ping", async (req, res, next) => {
@@ -24,6 +22,17 @@ app.use((req, res, next) => {
 });
 
 app.use(errorHandler);
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught exception:", error);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+  process.exit(1);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
