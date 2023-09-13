d877cf50c7dde777013ff0b68d3058c87723dcf2
qvadricepc <qvadricepc@gmail.com>
2023-09-13 15:05:49 +0300

    feat: refactor

--- src/modules/errors/controllers/__tests__/bad-implementation.spec.ts
+++ src/modules/errors/controllers/__tests__/bad-implementation.spec.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import * as Boom from "@hapi/boom";
 import { badImplementation } from "../bad-implementation";
 
