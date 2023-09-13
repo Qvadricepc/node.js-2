82620b07f1acd877ac663df286d4cf49f58a63c0
qvadricepc <qvadricepc@gmail.com>
2023-09-13 13:49:02 +0300

    feat: add typescript and architecture update

--- src/modules/auth/controllers/auth.ts
+++ src/modules/auth/controllers/auth.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 
 exports.auth = async (req: Request, res: Response) => {
   return res.json({ message: "login" });
